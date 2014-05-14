2e4076525c7b029d564f984d83e7563a53e42949
MS <martin.schrimpf@tum.de>
2014-05-14 07:24:38 +0200

    pthread_mutex_lock version of helper

--- helpers/pthread_lock.cpp
+++ helpers/pthread_lock.cpp
@@ -3,23 +3,26 @@
 #include <pthread.h>
 #include <immintrin.h>
 #include <vector>
+#include <unistd.h> // usleep
 
-#include "../util.h"
 #include "../hle/lib/hle-emulation.h"
 
 int main(int argc, char *argv[]) {
 	usleep(5 * 100000); // allow perf to catch up
 
+	int loops = 1000000;
+	int add = 5;
+
 	pthread_mutex_t mutex;
 	mutex = PTHREAD_MUTEX_INITIALIZER; // default init
 
 	int n = 0;
-	for (int i = 0; i < 100000; i++) {
+	for (int i = 0; i < loops; i++) {
 		pthread_mutex_lock(&mutex);
-		n += 5;
+		n += add;
 		pthread_mutex_unlock(&mutex);
 	}
-	printf("n: %d\n", n);
+	printf("n: %d (%s)\n", n, n == loops * add ? "correct" : "incorrect");
 
 	// force usage of libstdc++.so.6
 	std::vector<int> v;

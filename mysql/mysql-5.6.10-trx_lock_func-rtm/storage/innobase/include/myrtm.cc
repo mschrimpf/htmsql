f4108b7e011c4b7a5331633857f7dd3d76f29fb0
MS <martin.schrimpf@tum.de>
2014-05-23 11:07:59 +0200

    Missing files added

---
+++ mysql/mysql-5.6.10-trx_lock_func-rtm/storage/innobase/include/myrtm.cc
@@ -0,0 +1,19 @@
+#include <immintrin.h> // RTM
+#include <xmmintrin.h> // _mm_pause
+#include <stdio.h>     // print
+
+int rtm_lock() {
+	int failures = 0, failures_max = 100000;
+	retry: if (_xbegin() == _XBEGIN_STARTED) {
+		return 0;
+	} else if (++failures <= failures_max) {
+		goto retry;
+	} else {
+		printf("Max failure count (%d) reached\n", failures);
+		exit(1);
+		return 1;
+	}
+}
+void rtm_unlock() {
+	_xend();
+}
